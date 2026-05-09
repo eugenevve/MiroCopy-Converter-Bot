c6cf9106184cd58b10a49f1cd991fc7f0e0a6a51
Eugene <e.viktorovvv@gmail.com>
2026-05-10 00:02:02 +0600

    Renaming a photo section file to image

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,11 +1,11 @@
 from aiogram import Router
 from .common import router as common_router
-from .photo import router as photo_router
+from .image import router as image_router
 
 def get_handlers_router() -> Router:
     main_router = Router()
 
     main_router.include_router(common_router)
-    main_router.include_router(photo_router)
+    main_router.include_router(image_router)
     
     return main_router
