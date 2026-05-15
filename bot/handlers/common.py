00583121f64bbe42632cc2fa2c1524916a244a59
Eugene <e.viktorovvv@gmail.com>
2026-05-15 17:26:04 +0600

    Adding inline mode

--- bot/handlers/common.py
+++ bot/handlers/common.py
@@ -1,14 +1,16 @@
 import asyncio
+import datetime
 
 from aiogram import Router, types, F
 from aiogram.fsm.context import FSMContext
-from aiogram.filters import Command, StateFilter
+from aiogram.filters import Command, CommandObject, StateFilter
 
 from aiogram.types import ReplyKeyboardRemove
 from bot.keyboards.menu import get_menu_keyboard
 from bot.keyboards.back import get_back_keyboard
 from bot.locales.index import get_texts
 from bot.ui.errors import send_unsupported_content
+from bot.utils.enums import CommandArgs
 from bot.utils.states import ConvertStates
 
 
@@ -18,10 +20,33 @@ router = Router()
 # ---------------------------
 # START
 # ---------------------------
+last_starts = {}
+
 @router.message(Command("start"))
-async def cmd_start(message: types.Message, state: FSMContext):
-    await state.set_state(ConvertStates.main_menu)
+async def cmd_start(message: types.Message, state: FSMContext, command: CommandObject):
+    user_id = message.from_user.id
+    now = datetime.datetime.now()
+
+    # Problem: On Apple devices due to the way deeplinks work in the client.
+    # The system may initiate a transition from inline mode twice.
+    # First, a preview occurs then the actual opening resulting in two commands being sent.
+    if user_id in last_starts:
+        delta = (now - last_starts[user_id]).total_seconds()
+        if delta < 1.0:
+            return
+    last_starts[user_id] = now
+
+    if command.args == CommandArgs.IMAGE:
+        return await set_image_mode(message, state)
+    elif command.args == CommandArgs.TXT:
+        return await set_txt_mode(message, state)
+    else:
+        return await show_main_menu(message, state)
+
+
+async def show_main_menu(message: types.Message, state: FSMContext):
     lang = message.from_user.language_code if message.from_user else None
+    await state.set_state(ConvertStates.main_menu)
     await message.answer(
         get_texts(lang).START,
         reply_markup=get_menu_keyboard(lang)
@@ -32,6 +57,7 @@ async def cmd_start(message: types.Message, state: FSMContext):
 # MODES
 # ---------------------------
 @router.message(StateFilter(ConvertStates.main_menu), F.text.contains("📸"))
+@router.message(Command("image"))
 async def set_image_mode(message: types.Message, state: FSMContext):
     await state.set_state(ConvertStates.convert_for_images)
     lang = message.from_user.language_code if message.from_user else None
@@ -42,7 +68,8 @@ async def set_image_mode(message: types.Message, state: FSMContext):
 
 
 @router.message(StateFilter(ConvertStates.main_menu), F.text.contains("🗒"))
-async def set_image_mode(message: types.Message, state: FSMContext):
+@router.message(Command("txt"))
+async def set_txt_mode(message: types.Message, state: FSMContext):
     await state.set_state(ConvertStates.convert_for_txt)
     lang = message.from_user.language_code if message.from_user else None
     await message.answer(
